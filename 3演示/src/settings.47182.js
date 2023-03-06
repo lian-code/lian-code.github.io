dc37c9f703d2793ed875f3b79f9ace27a18e8bfe
lian_code <77065394+lian-code@users.noreply.github.com>
2023-03-06 14:18:37 +0800

    Add files via upload

---
+++ 3演示/src/settings.47182.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/ind.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",resources:"2fe43",main:"42cf1"}};
